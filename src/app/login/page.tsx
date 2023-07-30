ed0dfe8e0720e79f527e7fc6b96ef6e29f382be9
Anshu Kumar Singh <anshusinghh29@gmail.com>
2023-07-30 09:13:19 +0530

    login-completed

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,16 +1,34 @@
 "use client"
 import { useRouter } from "next/navigation"
-import React from "react"
+import {useState,useEffect} from "react"
 import Link from "next/link"
+import axios from "axios"
 
 export default function Login(){
-    const {user,setUser}=React.useState({
+    const router=useRouter()
+    const [user,setUser]=useState({
         email:"",
         password:"",
     })
+    const [loading,setLoading]=useState(false)
+
+    const Onlogin=async (e:any)=>{
+        e.preventDefault();
+        try {
+            setLoading(true)
+            const res=await axios.post("/api/user/login",user)    
+            console.log("LoginConfirmed",res.data)
+            router.push("/profile")       
+        } catch (error:any) {
+            console.log("Login failed", error.message);
+            
+        }finally{
+            setLoading(false)
+        }
+    }
     return (
         <div>
-            <h1>Login</h1>
+            <h1>{loading?"processing":"Login"}</h1>
             <form  method="get" className="flex flex-col">
                 <input type="email" placeholder="email" 
                         onChange={(e)=>{
@@ -19,8 +37,14 @@ export default function Login(){
                                 email:e.target.value
                             })
                         }}/>
-                <input type="password" placeholder="password" />
-                <button >Login</button>
+                <input type="password" placeholder="password" 
+                onChange={(e)=>{
+                    setUser({
+                        ...user,
+                        password:e.target.value
+                    })
+                }}/>
+                <button onClick={Onlogin}>Login</button>
             </form>
             <Link href="/signup" className="bg-purple-600">Signup Page</Link>
         </div>
