343d7ff988f9ca2ed3049e89db52a3890f4b7689
Anshu Kumar Singh <anshusinghh29@gmail.com>
2023-07-30 16:36:16 +0530

    logout function added

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,10 +1,27 @@
+"use client"
+
+import axios from "axios"
+import { useRouter } from "next/navigation"
+import {useState,useEffect} from "react"
 
 export default function profilepage(){
+
+    const router=useRouter()
+    const Onlogout=async()=>{
+        try {
+            await axios.get("/api/user/logout")
+            router.push("/login")
+        } catch (error:any) {
+            console.log(error.message)
+        }
+    }
     return (
         <div>
             <h1>Profile</h1>
             <hr />
             <p>profile content</p>
+            <hr />
+            <button onClick={Onlogout}>Logout</button>
         </div>
     )
 }
